362c1bba50c8f4211d3b707912e387d046dd4df4
Utkarsh Gupta <79002186+utkarsh-1912@users.noreply.github.com>
2022-07-14 14:39:40 +0530

    Version : 0.0.1

---
+++ Task 4/src/component/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="item">
+        <img src="./images/logo.jpg" alt="logo" />
+      </div>
+      <div className="item">
+        <p>VIP | TASK 4 | Calculator</p>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
